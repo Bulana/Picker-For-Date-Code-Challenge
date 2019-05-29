1a14345eb5b65f231a08b49d2fba391880051015
Bulana <Bulana@hal9000.zapper.local>
2019-05-29 10:36:16 +0200

    code refactored

--- app/src/main/java/com/bulana/pickerfordate/MainActivity.java
+++ app/src/main/java/com/bulana/pickerfordate/MainActivity.java
@@ -5,14 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.bulana.pickerfordate.constant.Constant;
 
+public class MainActivity extends AppCompatActivity {
+    private TextView dateTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dateTextView = findViewById(R.id.textView);
     }
 
     public void showDatePicker(View view) {
@@ -20,11 +25,11 @@ public class MainActivity extends AppCompatActivity {
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
     public void processDatePickerResult(int year, int month, int day) {
-        String month_string = Integer.toString(month+1);
+        String month_string = Integer.toString(month+Constant.ONE);
         String day_string = Integer.toString(day);
         String year_string = Integer.toString(year);
-        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
-
+        String dateMessage = (month_string + Constant.FORWARD_SLASH + day_string + Constant.FORWARD_SLASH + year_string);
+        dateTextView.setText(dateMessage);
         Toast.makeText(this, "Date: " + dateMessage, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/bulana/pickerfordate/constant/Constant.java
@@ -0,0 +1,6 @@
+package com.bulana.pickerfordate.constant;
+
+public class Constant {
+    public final static String FORWARD_SLASH = "/";
+    public final static int ONE = 1;
+}
